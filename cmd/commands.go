bd3c85643ce82372fef5be25195e5185c69bb328
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-08-13 02:36:23 -0300

    feat: added cobra library to provide a simple CLI

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,11 +1,71 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/mdcg/go-port-scanner/scanner"
+	"github.com/spf13/cobra"
+)
 
-func PortScannerRootCommand() *cobra.Command {
-	return &cobra.Command{
-		Use:   "Port Scanner",
-		Short: "List Ports",
-		Long:  "List all TCP Opened ports",
+func Execute() {
+	var host string
+	var port int
+
+	rootCmd := &cobra.Command{
+		Use: "scan",
 	}
+
+	rootCmd.PersistentFlags().StringVar(
+		&host, "host", "localhost",
+		"Host you want to scan",
+	)
+	rootCmd.PersistentFlags().IntVar(
+		&port, "ports", 10000,
+		"Number of ports you want to scan.",
+	)
+
+	cmdTCPPortScanner := &cobra.Command{
+		Use:   "tcp",
+		Short: "Initiates scanning of TCP ports.",
+		Long: `
+			Initiates scanning of TCP ports on a given host.
+			Use the --host flags to indicate which host will be scanned,
+			and --ports to indicate the number of ports to be scanned.
+		`,
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner.FormatScanReport(scanner.TCPScanner(host, port))
+		},
+	}
+
+	cmdUDPPortScanner := &cobra.Command{
+		Use:   "udp",
+		Short: "Initiates scanning of UDP ports.",
+		Long: `
+			Initiates scanning of UDP ports on a given host.
+			Use the --host flags to indicate which host will be scanned,
+			and --ports to indicate the number of ports to be scanned.
+		`,
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner.FormatScanReport(scanner.UDPScanner(host, port))
+		},
+	}
+
+	cmdFullPortScanner := &cobra.Command{
+		Use:   "fullscan",
+		Short: "Starts scanning all ports, whether they are TCP or UDP.",
+		Long: `
+			Initiates scanning all ports, whether TCP or UDP, on a given host.
+			Use the --host flags to indicate which host will be scanned,
+			and --ports to indicate the number of ports to be scanned.
+		`,
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner.FormatScanReport(scanner.FullScanner(host, port))
+		},
+	}
+
+	rootCmd.AddCommand(cmdTCPPortScanner)
+	rootCmd.AddCommand(cmdUDPPortScanner)
+	rootCmd.AddCommand(cmdFullPortScanner)
+	rootCmd.Execute()
 }
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
-import (
-	"github.com/mdcg/go-port-scanner/scanner"
-)
+import "github.com/mdcg/go-port-scanner/cmd"
 
 func main() {
 	// scanner.FormatScanReport(scanner.TCPScanner("localhost", 10000))
 	// scanner.FormatScanReport(scanner.UDPScanner("localhost", 10000))
-	scanner.FormatScanReport(scanner.FullScanner("localhost", 10000))
+	// scanner.FormatScanReport(scanner.FullScanner("localhost", 10000))
+	cmd.Execute()
 }
